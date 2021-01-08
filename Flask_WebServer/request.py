11038a4449265f715dfb727f690635d0c37bf1e7
Kevin Lim <kevin.lim@edu.devinci.fr>
2021-01-08 22:21:00 +0100

    upd

--- Flask_WebServer/request.py
+++ Flask_WebServer/request.py
@@ -2,8 +2,8 @@ import requests
 import json
 import pickle as p
 import pandas as pd
-
-url = 'http://0.0.0.0:5000/api/'
+import csv
+url = 'http://127.0.0.1:5000/api/'
 
 with open('model.pickle', 'rb') as f_model:
     model = p.load(f_model) # trained classifier model
@@ -12,12 +12,11 @@ with open('column_trans.pickle', 'rb') as f_onehotenc:
 with open('scaler.pickle', 'rb') as f_scaler:
     scaler = p.load(f_scaler) # OneHotEncoder object that was used in training
 
-data = [[0,0,0,0,1,0,0.2,0.2,0,0,"Feb",1,1,1,1,"Returning_Visitor","FALSE"]]
-df = pd.DataFrame(data=data)
+df = pd.read_csv("request.csv")
 df = ohe.transform(df)
 df = scaler.transform(df)
-
-j_data = json.dumps(data)
+df = df.tolist()
+j_data = json.dumps(df)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 r = requests.post(url, data=j_data, headers=headers)
 print(r, r.text)
