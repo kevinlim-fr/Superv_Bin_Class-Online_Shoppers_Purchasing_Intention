028751f0408da0d88e0bce614aa2fe8190c0f4c2
kevinlim <49164343+kekelim@users.noreply.github.com>
2021-01-08 22:08:21 +0100

    upd

--- Flask_WebServer/request.py
+++ Flask_WebServer/request.py
@@ -1,18 +1,21 @@
 import requests
 import json
+import pickle as p
+import pandas as pd
 
 url = 'http://0.0.0.0:5000/api/'
 
 with open('model.pickle', 'rb') as f_model:
-    model = pickle.load(f_model) # trained classifier model
+    model = p.load(f_model) # trained classifier model
 with open('column_trans.pickle', 'rb') as f_onehotenc:
-    ohe = pickle.load(f_onehotenc) # OneHotEncoder object that was used in training
+    ohe = p.load(f_onehotenc) # OneHotEncoder object that was used in training
 with open('scaler.pickle', 'rb') as f_scaler:
-    scaler = pickle.load(f_scaler) # OneHotEncoder object that was used in training    
+    scaler = p.load(f_scaler) # OneHotEncoder object that was used in training
 
-data = [[0,0,0,0,1,0,0.2,0.2,0,0,Feb,1,1,1,1,Returning_Visitor,FALSE]]
-data = ohe.transform(data)
-data = scaler.transform(data)
+data = [[0,0,0,0,1,0,0.2,0.2,0,0,"Feb",1,1,1,1,"Returning_Visitor","FALSE"]]
+df = pd.DataFrame(data=data)
+df = ohe.transform(df)
+df = scaler.transform(df)
 
 j_data = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
